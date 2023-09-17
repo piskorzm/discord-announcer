812dcf3bf580cb311a957253948ba02cd4c4a3ce
Marcin Piskorz <marcinpiskorz01@gmail.com>
2023-09-17 02:50:27 +0200

    Sounds downloaded and trimmed locally

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const { Client, GatewayIntentBits } = require('discord.js');
 const { joinVoiceChannel, createAudioPlayer, createAudioResource, NoSubscriberBehavior } = require('@discordjs/voice');
 const ytdl = require('ytdl-core');
 const fs = require('fs');
+const ffmpeg = require('fluent-ffmpeg');
 
-const DEFAULT_SOUND = 'https://www.youtube.com/watch?v=10LsX1o9An0&ab_channel=Flank3RR';
-const TIMEOUT_MS = 7000;
-const REGISTERED_SOUNDS_FILE_PATH = './registered-sounds.json';
+const AUDIO_CLIPS_PATH = './audio-clips';
+const DEFAULT_SOUND = AUDIO_CLIPS_PATH + '/default.mp4';
+const MAX_CLIP_DURATION = 10;
 
 const client = new Client({
   intents: [
@@ -18,15 +19,12 @@ const client = new Client({
   ],
 });
 
-const registeredSounds = (() => {
-  try {
-    const data = fs.readFileSync(REGISTERED_SOUNDS_FILE_PATH);
-    return JSON.parse(data);
-  } catch (error) {
-    console.error('Error loading registered sounds:', error);
-    return {};
-  }
-})();
+const connections = new Map();
+
+// Create the tempVideoFolder if it doesn't exist
+if (!fs.existsSync(AUDIO_CLIPS_PATH)) {
+  fs.mkdirSync(AUDIO_CLIPS_PATH);
+}
 
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
@@ -43,11 +41,17 @@ client.on('messageCreate', async (message) => {
 
   if (command === '!add-sound') {
     const youtubeUrl = args.length >= 1 ? args[0] : undefined;
+    const startTime = args.length >= 2 ? parseFloat(args[1]) : undefined;
+    const duration = args.length >= 3 && !!startTime && !!parseFloat(args[2]) ? parseFloat(args[2]) - startTime : undefined;
 
     // Validate URL argument syntax
     if (!youtubeUrl || !ytdl.validateURL(youtubeUrl)) {
       message.reply('Please provide a valid YouTube URL.');
       return;
+    }
+
+    if (!!duration && duration < 0) {
+      message.reply('End time can not be before start time.');
     }
 
     // Fetch video info to further verify if the video exists
@@ -58,31 +62,100 @@ client.on('messageCreate', async (message) => {
         return;
       }
 
-      // Update the registeredSounds map
-      registeredSounds[message.author.tag] = youtubeUrl;
+      // Download the video using ytdl-core
+      const fullAudioPath = `${AUDIO_CLIPS_PATH}/full_${message.author.tag}.mp4`; 
+      
+      const trimmedAudioFilePath = `${AUDIO_CLIPS_PATH}/${message.author.tag}.mp4`;
 
-      // Save the updated data to the JSON file
-      fs.writeFileSync(REGISTERED_SOUNDS_FILE_PATH, JSON.stringify(registeredSounds));
+      message.reply('Downloading audio clip...');
+      const audioFile = ytdl(youtubeUrl, { filter: 'audioonly', format: 'mp4'});
+      audioFile.pipe(fs.createWriteStream(fullAudioPath));
+      audioFile.on('progress', (chunkLength, downloaded, total) => {
+        const percent = downloaded / total * 100;
+        process.stdout.clearLine();
+        process.stdout.cursorTo(0);
+        process.stdout.write(`Downloading... ${percent.toFixed(2)}%`);
+      });
+      audioFile.on('finish', () => {
+            
+      console.error('finish:');
+      // Create an FFmpeg command
+      const command = ffmpeg();
 
-      console.log(`Sound registered for user: ${message.author.tag}, url: ${youtubeUrl}`);
-      message.reply('Your welcome sound has been registered!');
+      // Set input file
+      command.input(fullAudioPath);
+
+      command.setStartTime(!!startTime ? startTime : 0);
+      command.setDuration(duration && duration < MAX_CLIP_DURATION ? duration : MAX_CLIP_DURATION)
+
+      // Set output file path
+      command.output(trimmedAudioFilePath);
+
+      // Run the FFmpeg command
+      command.on('end', () => {
+        console.log('Trimming finished');
+        console.log(`Sound registered for user: ${message.author.tag}, url: ${youtubeUrl}`);
+        message.reply('Your welcome sound has been registered!');
+
+        // Remove the full audio
+        fs.unlink(fullAudioPath, (error) => {
+          if (error) {
+            console.error('Error while deleting the file:', error);
+          } else {
+            console.log('File deleted successfully.');
+          }
+        });
+      })
+      .on('error', (err) => {
+        message.reply('An error occurred while trimming .');
+        console.error('Error:');
+      })
+      .run();
+    });
+    audioFile.on('error', (err) => {
+      message.reply('An error occurred while downloading audio clip.');
+      console.error('Error during download:', err);
+    })
+          
     } catch (error) {
       console.error(error);
-      message.reply('An error occurred while fetching video information.');
+      message.reply('An error occurred while registering audio clip.');
     }
   }
 });
 
 client.on('voiceStateUpdate', async (oldState, newState) => {
-  if (!oldState.channel && newState.channel) {
+
+  const channelId = newState.channelId || oldState.channelId;
+  const channel = client.channels.cache.get(channelId);
+
+  if (!channel) {
+    console.error('Channel not found');
+    return;
+  }
+
+  // Check if there are no users left in the channel
+  const membersInChannel = channel.members.filter(member => !member.user.bot).size;
+  console.log(membersInChannel)
+
+  if (membersInChannel === 0 && !!connections.get(channelId)) {
+    // Disconnect the bot from the channel
+    console.log(client.voice);
+    connections.get(channelId).destroy();
+    connections.delete(channelId);
+    console.log(`Bot disconnected from ${channel.name} due to no users.`);
+    return;
+  }
+
+  if (newState.channel) {
     // User joined a voice channel
     if (newState.member.user.bot) {
       // Ignore bot users
       return;
     }
 
-    let registeredSound = !!registeredSounds[newState.member.user.tag] ?
-        registeredSounds[newState.member.user.tag] : DEFAULT_SOUND;
+    const userAudioPath = `${AUDIO_CLIPS_PATH}/${newState.member.user.tag}.mp4`;
+    const audioPath = fs.existsSync(userAudioPath) ? userAudioPath : DEFAULT_SOUND;
 
     const connection = joinVoiceChannel({
       channelId: newState.channel.id,
@@ -90,41 +163,24 @@ client.on('voiceStateUpdate', async (oldState, newState) => {
       adapterCreator: newState.guild.voiceAdapterCreator,
     });
 
+    connections.set(channelId, connection);
+
     const audioPlayer = createAudioPlayer({
       behaviors: {
         noSubscriber: NoSubscriberBehavior.Pause,
       },
     });
-
-    const stream = ytdl(registeredSound, { filter: 'audioonly' });
-
-    const audioResource = createAudioResource(stream, {
-      inlineVolume: true,
-    });
+          
+    const audioResource = createAudioResource(audioPath);
 
     audioPlayer.play(audioResource);
 
     connection.subscribe(audioPlayer);
 
-    // Schedule disconnect if the sound is too long
-    setTimeout(() => {
-      try {
-        connection.destroy();
-      } catch(e) {
-        console.log(e)
-      }
-    }, TIMEOUT_MS);
-
     audioPlayer.on('stateChange', (oldState, newState) => {
-      
-      console.log(connection.status)
-        if (newState.status === 'idle') {
-          try {
-            connection.destroy();
-          } catch(e) {
-            console.log(e)
-          }
-        }
+      if (newState.status === 'idle') {
+        audioPlayer.stop();
+      }
     });
   }
 });
