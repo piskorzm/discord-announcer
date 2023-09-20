8d84b57bbc45d7148ff2c305d69776a58239bd70
Marcin Piskorz <marcinpiskorz01@gmail.com>
2023-09-20 13:05:05 +0200

    Min max volume values changed to constants

--- commands/set-volume.js
+++ commands/set-volume.js
@@ -3,18 +3,20 @@ const { SlashCommandBuilder } = require('discord.js');
 const fs = require('fs');
 
 const VOLUME_OPTION = 'volume';
+const MIN_VOLUME = 0.01;
+const MAX_VOLUME = 5.0;
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('set-volume')
         .setDescription('Sets volume of your welcome sound!')
-        .addNumberOption(option => option.setName(VOLUME_OPTION).setDescription('Volume setting between 0.01 and 2.00').setRequired(true)),
+        .addNumberOption(option => option.setName(VOLUME_OPTION).setDescription(`Volume setting between ${MIN_VOLUME} and ${MAX_VOLUME}`).setRequired(true)),
     async execute(interaction) {
         const volume = interaction.options.get(VOLUME_OPTION).value;
 
         // Validate volume value
-        if (!volume || volume > 2.0 || volume < 0.01) {
-            interaction.reply('Please provide a number between 0.01 and 2.00');
+        if (!volume || volume < MIN_VOLUME || volume > MAX_VOLUME) {
+            interaction.reply(`Please provide a number between ${MIN_VOLUME} and ${MAX_VOLUME}`);
             return;
         }
 
